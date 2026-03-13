ffc80aa749ca9a24c022d7646d6a7182b09fd244
Saimum Rahman <saimumrahman1214@gmail.com>
2026-03-13 22:09:59 +0600

    purchase for modified

--- JM.UI.Entities/Model/Purchases/BarcodeGenerationRequestDTO.cs
+++ JM.UI.Entities/Model/Purchases/BarcodeGenerationRequestDTO.cs
@@ -9,6 +9,7 @@ namespace JM.UI.Entities.Model.Purchases
         public string? ShadeNo { get; set; }
         public string? ColorName { get; set; }
         public string? SizeName { get; set; }
+        public string? BarcodePrefix { get; set; }
         public int? ItemId { get; set; }
         public int? GroupId { get; set; }
     }
